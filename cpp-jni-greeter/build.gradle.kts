92f95c81ce4d4d25ff8feb494c94f45af30ed84c
Clayton Walker <clayton.m.walker@gmail.com>
2023-05-25 18:09:21 -0600

    Cleanup compile task and split out mac an non-mac compile args

--- cpp-jni-greeter/build.gradle.kts
+++ cpp-jni-greeter/build.gradle.kts
@@ -1,46 +1,59 @@
 import org.gradle.internal.jvm.Jvm
 
 plugins {
-	id("cpp-library")
+    `cpp-library`
 }
 
 description = "The JNI shared library, also known as the native bindings."
 
 library {
-	// The native component of the JNI library needs to be a shared library.
-	linkage.set(listOf(Linkage.SHARED))
-	dependencies {
-		implementation(project(":cpp-greeter"))
-	}
+    // The native component of the JNI library needs to be a shared library.
+    linkage.set(listOf(Linkage.SHARED))
+    dependencies {
+        implementation(project(":cpp-greeter"))
+    }
 
-	binaries.configureEach {
-		compileTask.get().includes.from(compileTask.get().targetPlatform.map {
-			listOf(File("${Jvm.current().javaHome.canonicalPath}/include")) + when {
-				it.operatingSystem.isMacOsX -> listOf(File("${Jvm.current().javaHome.absolutePath}/include/darwin"))
-				it.operatingSystem.isLinux -> listOf(File("${Jvm.current().javaHome.absolutePath}/include/linux"))
-				it.operatingSystem.isWindows -> listOf(File("${Jvm.current().javaHome.absolutePath}/include/win32"))
-				else -> emptyList()
-			}
-		})
-		compileTask.get().isPositionIndependentCode = true
-		compileTask.get().compilerArgs.addAll("-arch", "arm64", "-arch", "x86_64")
-		compileTask.get().includes.from(generatedHeaders)
-	}
+    binaries.configureEach {
+        val compileTask = compileTask.get()
+        compileTask.includes.from(compileTask.targetPlatform.map {
+            listOf(File("${Jvm.current().javaHome.canonicalPath}/include")) + when {
+                it.operatingSystem.isMacOsX -> listOf(File("${Jvm.current().javaHome.absolutePath}/include/darwin"))
+                it.operatingSystem.isLinux -> listOf(File("${Jvm.current().javaHome.absolutePath}/include/linux"))
+                it.operatingSystem.isWindows -> listOf(File("${Jvm.current().javaHome.absolutePath}/include/win32"))
+                else -> emptyList()
+            }
+        })
+        compileTask.isPositionIndependentCode = true
+        compileTask.compilerArgs.addAll(compileTask.targetPlatform.map {
+            when {
+                it.operatingSystem.isMacOsX -> listOf("-arch", "arm64", "-arch", "x86_64")
+                it.operatingSystem.isLinux -> emptyList()
+                it.operatingSystem.isWindows -> emptyList()
+                else -> emptyList()
+            }
+        })
+        compileTask.includes.from(generatedHeaders)
+    }
 }
 
 tasks.withType<LinkSharedLibrary>().configureEach {
-	// todo: allow this to only run on macos?
-	//  maybe also linux? or also cross-build on windows?
-	linkerArgs.addAll("-arch", "arm64", "-arch", "x86_64")
+    linkerArgs.addAll(
+        targetPlatform.map {
+            when {
+                it.operatingSystem.isMacOsX -> listOf("-arch", "arm64", "-arch", "x86_64")
+                it.operatingSystem.isLinux -> emptyList()
+                it.operatingSystem.isWindows -> emptyList()
+                else -> emptyList()
+            }
+        }
+    )
 }
 
 val generatedHeaders by configurations.creating {
-	isCanBeConsumed = false
-	isCanBeResolved = true
+    isCanBeConsumed = false
+    isCanBeResolved = true
 }
 
 dependencies {
-	generatedHeaders(project(mapOf(
-		"path" to ":java-jni-greeter",
-		"configuration" to "generatedHeaders")))
+    generatedHeaders(project(path = ":java-jni-greeter", configuration = "generatedHeaders"))
 }
