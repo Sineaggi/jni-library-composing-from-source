e00b3f43934f2ebf28d96989deafdf6237f6d478
Clayton Walker <clayton.m.walker@gmail.com>
2023-05-27 14:42:33 -0600

    Replace tabs with spaces

--- cpp-greeter/src/main/cpp/greeter_impl.cpp
+++ cpp-greeter/src/main/cpp/greeter_impl.cpp
@@ -4,5 +4,5 @@
 #include <string>
 
 std::string say_hello(std::string name) {
-	return "Bonjour, " + name + "!";
+    return "Bonjour, " + name + "!";
 }
--- java-jni-greeter/build.gradle.kts
+++ java-jni-greeter/build.gradle.kts
@@ -1,20 +1,20 @@
 plugins {
-	id("java-library")
+    id("java-library")
 }
 
 description = "The JNI classes, also known as the JVM bindings."
 
 dependencies {
-	implementation(project(":java-loader"))
+    implementation(project(":java-loader"))
 }
 
 val generatedHeaders by configurations.creating {
-	isCanBeConsumed = true
-	isCanBeResolved = false
+    isCanBeConsumed = true
+    isCanBeResolved = false
 }
 
 artifacts {
-	add(generatedHeaders.name, layout.buildDirectory.dir("generated/sources/headers/java/main")) {
-		builtBy(tasks.classes)
-	}
+    add(generatedHeaders.name, layout.buildDirectory.dir("generated/sources/headers/java/main")) {
+        builtBy(tasks.classes)
+    }
 }
